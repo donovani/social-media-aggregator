532ece5eeae175ea5acdd59dc5d820b33d12fa96
Jake <hallja99@gmail.com>
2017-06-25 23:33:10 -0400

    Minor Bug Fixes
    - Split Feed from 1 colum to 2
         - col 1 - My Posts
         - col 2 - Posts im tagged int
    
    - Changed number from 10 to 25 for both colums
    - commented out code put in place for future public only filter

--- src/js/fbHome.js
+++ src/js/fbHome.js
@@ -1,56 +1,8 @@
 function statusChangeCallback(response) {
     if (response.status === 'connected') {
-        // Logged into your app and Facebook.
-        FB.api(
-            "/me/feed?limit=10",
-            function(response) {
-                if (response && !response.error) {
-                    posts = response.data
+     getUserPosts();
 
-
-                    for (var i = 0; i < posts.length; i++) {
-                        id = posts[i].id.split("_")
-                        var cntr = document.createElement("center")
-                        var frm = document.createElement("iframe")
-
-
-                        frm.setAttribute('src', "https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2F" + id[0] + "%2Fposts%2F" + id[1] + "%2F&show_text=true&appId=1896793147252358")
-                        frm.setAttribute('width', "95%")
-                        frm.setAttribute('height', "100%")
-                        frm.setAttribute('style', 'border:none;overflow:scroll')
-                        frm.setAttribute('scrolling', 'no')
-                        frm.setAttribute('frameborder', '.1')
-                        frm.setAttribute('allowTransparency', true)
-
-                        var element = document.getElementById("posts")
-
-                        cntr.appendChild(frm)
-                        element.appendChild(cntr)
-
-                        console.log("written");
-                        (function(d, s, id) {
-                            var js, fjs = d.getElementsByTagName(s)[0];
-                            if (d.getElementById(id))
-                                return;
-                            js = d.createElement(s);
-                            js.id = id;
-                            js.src =
-                                "//connect.facebook.net/en_US/all.js#xfbml=1&appId=135669679827333";
-                            fjs.parentNode.insertBefore(js, fjs);
-                        }(document, 'script',
-                            'facebook-jssdk'));
-                    }
-
-
-                } else {
-                    console.log("Error")
-                }
-
-            });
-
-        testAPI();
-
-    } else {
+ } else {
         //IF NOT LOGGED INTO OTHER SOCIAL MEDIA
         window.location = "index.html"
     }
@@ -90,3 +42,112 @@ function testAPI() {
         console.log("Success")
     });
 }
+
+
+
+function getUserPosts(){
+     // Logged into your app and Facebook.
+     FB.api(
+        "/me/posts?limit=25",
+        function(response) {
+            if (response && !response.error) {
+                posts = response.data
+                for (var i = 0; i < posts.length; i++) {
+                    
+                    /*var index = 0
+                    FB.api(
+                        "/" + posts[i].id,
+                        function (response2) {
+                          if (response2 && !response2.error) {
+                           // console.log(response.icon)
+                            console.log(response2.privacy.value)
+                            index ++;
+                        }
+                        else{
+                            console.log(response2.error)
+                        }
+                    });
+                    console.log(index)*/
+                         id = posts[i].id.split("_")
+                        var cntr = document.createElement("center")
+                        var frm = document.createElement("iframe")
+
+
+                        frm.setAttribute('src', "https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2F" + id[0] + "%2Fposts%2F" + id[1] + "%2F&show_text=true&appId=1896793147252358")
+                        frm.setAttribute('width', "95%")
+                        frm.setAttribute('height', "500px")
+                        frm.setAttribute('style', 'border:none;overflow:scroll')
+                        frm.setAttribute('allowTransparency', false)
+
+                        var element = document.getElementById("myPosts")
+
+                        cntr.appendChild(frm)
+                        element.appendChild(cntr)
+
+                        console.log("written");
+                        (function(d, s, id) {
+                            var js, fjs = d.getElementsByTagName(s)[0];
+                            if (d.getElementById(id))
+                                return;
+                            js = d.createElement(s);
+                            js.id = id;
+                            js.src =
+                            "//connect.facebook.net/en_US/all.js#xfbml=1&appId=135669679827333";
+                            fjs.parentNode.insertBefore(js, fjs);
+                        }(document, 'script',
+                            'facebook-jssdk'));
+
+                        }
+
+
+                    } else {
+                        console.log("Error")
+                    }
+
+                });
+
+         // Logged into your app and Facebook
+         FB.api(
+            "/me/tagged?limit=25",
+            function(response) {
+                if (response && !response.error) {
+                    posts = response.data
+
+                    for (var i = 0; i < posts.length; i++) {
+                        id = posts[i].id.split("_")
+                        var cntr = document.createElement("center")
+                        var frm = document.createElement("iframe")
+
+
+                        frm.setAttribute('src', "https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2F" + id[0] + "%2Fposts%2F" + id[1] + "%2F&show_text=true&appId=1896793147252358")
+                        frm.setAttribute('width', "95%")
+                        frm.setAttribute('height', "500px")
+                        frm.setAttribute('style', 'border:none;overflow:scroll')
+                        frm.setAttribute('allowTransparency', false)
+
+                        var element = document.getElementById("postsOfMe")
+
+                        cntr.appendChild(frm)
+                        element.appendChild(cntr)
+
+                        console.log("written");
+                        (function(d, s, id) {
+                            var js, fjs = d.getElementsByTagName(s)[0];
+                            if (d.getElementById(id))
+                                return;
+                            js = d.createElement(s);
+                            js.id = id;
+                            js.src =
+                            "//connect.facebook.net/en_US/all.js#xfbml=1&appId=135669679827333";
+                            fjs.parentNode.insertBefore(js, fjs);
+                        }(document, 'script',
+                            'facebook-jssdk'));
+                    }
+
+
+                } else {
+                    console.log("Error")
+                }
+
+            });
+     }
