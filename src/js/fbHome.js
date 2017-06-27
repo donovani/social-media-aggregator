b863af3407677cfa3ec542a0e46f6d04a1b9a36c
Jake <hallja99@gmail.com>
2017-06-27 14:38:27 -0400

    Code Cleanup
    Removed unnecessary code that was re calling the facebook SDK

--- src/js/fbHome.js
+++ src/js/fbHome.js
@@ -68,86 +68,58 @@ function getUserPosts(){
                         }
                     });
                     console.log(index)*/
-                         id = posts[i].id.split("_")
-                        var cntr = document.createElement("center")
-                        var frm = document.createElement("iframe")
+                    id = posts[i].id.split("_")
+                    var cntr = document.createElement("center")
+                    var frm = document.createElement("iframe")
 
 
-                        frm.setAttribute('src', "https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2F" + id[0] + "%2Fposts%2F" + id[1] + "%2F&show_text=true&appId=1896793147252358")
-                        frm.setAttribute('width', "95%")
-                        frm.setAttribute('height', "500px")
-                        frm.setAttribute('style', 'border:none;overflow:scroll')
-                        frm.setAttribute('allowTransparency', false)
+                    frm.setAttribute('src', "https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2F" + id[0] + "%2Fposts%2F" + id[1] + "%2F&show_text=true&appId=1896793147252358")
+                    frm.setAttribute('width', "95%")
+                    frm.setAttribute('height', "500px")
+                    frm.setAttribute('style', 'border:none;overflow:scroll')
+                    frm.setAttribute('allowTransparency', false)
 
-                        var element = document.getElementById("myPosts")
+                    var element = document.getElementById("myPosts")
+                    cntr.appendChild(frm)
+                    element.appendChild(cntr)
 
-                        cntr.appendChild(frm)
-                        element.appendChild(cntr)
+                    console.log("written");
+                }
 
-                        console.log("written");
-                        (function(d, s, id) {
-                            var js, fjs = d.getElementsByTagName(s)[0];
-                            if (d.getElementById(id))
-                                return;
-                            js = d.createElement(s);
-                            js.id = id;
-                            js.src =
-                            "//connect.facebook.net/en_US/all.js#xfbml=1&appId=135669679827333";
-                            fjs.parentNode.insertBefore(js, fjs);
-                        }(document, 'script',
-                            'facebook-jssdk'));
+            } else {
+                console.log("Error")
+            }
 
-                        }
-
-
-                    } else {
-                        console.log("Error")
-                    }
-
-                });
+        });
 
          // Logged into your app and Facebook
          FB.api(
             "/me/tagged?limit=25",
-            function(response) {
-                if (response && !response.error) {
-                    posts = response.data
-
-                    for (var i = 0; i < posts.length; i++) {
-                        id = posts[i].id.split("_")
-                        var cntr = document.createElement("center")
-                        var frm = document.createElement("iframe")
-
-
-                        frm.setAttribute('src', "https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2F" + id[0] + "%2Fposts%2F" + id[1] + "%2F&show_text=true&appId=1896793147252358")
-                        frm.setAttribute('width', "95%")
-                        frm.setAttribute('height', "500px")
-                        frm.setAttribute('style', 'border:none;overflow:scroll')
-                        frm.setAttribute('allowTransparency', false)
-
-                        var element = document.getElementById("postsOfMe")
-
-                        cntr.appendChild(frm)
-                        element.appendChild(cntr)
-
-                        console.log("written");
-                        (function(d, s, id) {
-                            var js, fjs = d.getElementsByTagName(s)[0];
-                            if (d.getElementById(id))
-                                return;
-                            js = d.createElement(s);
-                            js.id = id;
-                            js.src =
-                            "//connect.facebook.net/en_US/all.js#xfbml=1&appId=135669679827333";
-                            fjs.parentNode.insertBefore(js, fjs);
-                        }(document, 'script',
-                            'facebook-jssdk'));
-                    }
+           function(response) {
+            if (response && !response.error) {
+                posts = response.data
+                for (var i = 0; i < posts.length; i++) {
+                    id = posts[i].id.split("_")
+                    var cntr = document.createElement("center")
+                    var frm = document.createElement("iframe")
 
 
-                } else {
-                    console.log("Error")
+                    frm.setAttribute('src', "https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2F" + id[0] + "%2Fposts%2F" + id[1] + "%2F&show_text=true&appId=1896793147252358")
+                    frm.setAttribute('width', "95%")
+                    frm.setAttribute('height', "500px")
+                    frm.setAttribute('style', 'border:none;overflow:scroll')
+                    frm.setAttribute('allowTransparency', false)
+
+                    var element = document.getElementById("postsOfMe")
+                    cntr.appendChild(frm)
+                    element.appendChild(cntr)
+
+                    console.log("written");
                 }
 
-            });
+            } else {
+                console.log("Error")
+            }
+
+        });
      }
