9fa78e37648c7f5a590b3f822ac64bc715d449f9
Jake <hallja99@gmail.com>
2017-06-27 16:12:40 -0400

    Filter and Code Cleanup
    Added in a filter for public posts only, if there are private posts, a message will show how many are omitted
    
    code was modularized and broken down into two more fuctions to make it easier to read

--- src/js/fbHome.js
+++ src/js/fbHome.js
@@ -1,8 +1,8 @@
 function statusChangeCallback(response) {
     if (response.status === 'connected') {
-     getUserPosts();
+       getUserPosts();
 
- } else {
+   } else {
         //IF NOT LOGGED INTO OTHER SOCIAL MEDIA
         window.location = "index.html"
     }
@@ -48,42 +48,35 @@ function testAPI() {
 function getUserPosts(){
      // Logged into your app and Facebook.
      FB.api(
-        "/me/posts?limit=25",
+        "/me/posts?limit=100",
+        {fields: 'privacy'},
         function(response) {
             if (response && !response.error) {
                 posts = response.data
+
+                var index = 0;
+                var unable = false
+                var skipped = 0
+
                 for (var i = 0; i < posts.length; i++) {
-                    
-                    /*var index = 0
-                    FB.api(
-                        "/" + posts[i].id,
-                        function (response2) {
-                          if (response2 && !response2.error) {
-                           // console.log(response.icon)
-                            console.log(response2.privacy.value)
+                    if(index == 25){
+                        break;
+                    }
+                    else{
+                        if(posts[i].privacy.value == "EVERYONE"){
                             index ++;
+
+                            postPost("myPosts", posts[i])
                         }
                         else{
-                            console.log(response2.error)
+                            unable =true
+                            skipped ++;
                         }
-                    });
-                    console.log(index)*/
-                    id = posts[i].id.split("_")
-                    var cntr = document.createElement("center")
-                    var frm = document.createElement("iframe")
+                    } 
+                }
 
-
-                    frm.setAttribute('src', "https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2F" + id[0] + "%2Fposts%2F" + id[1] + "%2F&show_text=true&appId=1896793147252358")
-                    frm.setAttribute('width', "95%")
-                    frm.setAttribute('height', "500px")
-                    frm.setAttribute('style', 'border:none;overflow:scroll')
-                    frm.setAttribute('allowTransparency', false)
-
-                    var element = document.getElementById("myPosts")
-                    cntr.appendChild(frm)
-                    element.appendChild(cntr)
-
-                    console.log("written");
+                if(unable){
+                    postErrorMsg("myPosts", skipped)
                 }
 
             } else {
@@ -92,29 +85,36 @@ function getUserPosts(){
 
         });
 
-         // Logged into your app and Facebook
-         FB.api(
-            "/me/tagged?limit=25",
-           function(response) {
+     FB.api(
+        "/me/tagged?limit=100",
+        {fields: 'privacy'},
+        function(response) {
             if (response && !response.error) {
                 posts = response.data
+
+                var index = 0;
+                var unable = false
+                var skipped = 0
+
                 for (var i = 0; i < posts.length; i++) {
-                    id = posts[i].id.split("_")
-                    var cntr = document.createElement("center")
-                    var frm = document.createElement("iframe")
+                    if(index == 25){
+                        break;
+                    }
+                    else{
+                        if(posts[i].privacy.value == "EVERYONE"){
+                            index ++;
 
+                            postPost("postsOfMe", posts[i])
+                        }
+                        else{
+                            unable =true
+                            skipped ++;
+                        }
+                    } 
+                }
 
-                    frm.setAttribute('src', "https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2F" + id[0] + "%2Fposts%2F" + id[1] + "%2F&show_text=true&appId=1896793147252358")
-                    frm.setAttribute('width', "95%")
-                    frm.setAttribute('height', "500px")
-                    frm.setAttribute('style', 'border:none;overflow:scroll')
-                    frm.setAttribute('allowTransparency', false)
-
-                    var element = document.getElementById("postsOfMe")
-                    cntr.appendChild(frm)
-                    element.appendChild(cntr)
-
-                    console.log("written");
+                if(unable){
+                    postErrorMsg("postsOfMe", skipped)
                 }
 
             } else {
@@ -122,4 +122,45 @@ function getUserPosts(){
             }
 
         });
-     }
+    }
+
+
+    function postPost(parent, post){
+        id = post.id.split("_")
+        var cntr = document.createElement("center")
+        cntr.style = "padding-bottom: 10px;"
+        var frm = document.createElement("iframe")
+
+
+        frm.setAttribute('src', "https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2F" + id[0] + "%2Fposts%2F" + id[1] + "%2F&show_text=true&appId=1896793147252358")
+        frm.setAttribute('width', "95%")
+        frm.setAttribute('height', "500px")
+        frm.setAttribute('style', 'border: 1px solid black;overflow:scroll;background-color:white;')
+        frm.setAttribute('allowTransparency', false)
+
+        var element = document.getElementById(parent)
+        cntr.appendChild(frm)
+        element.appendChild(cntr)
+    }
+
+    function postErrorMsg(parent, skipped){
+        var element = document.getElementById(parent)
+        var cntr = document.createElement("center")
+        var p = document.createElement("p")
+        p.style = "width:95%; height:100px;  border-top: 1px solid black; border-bottom: 1px solid black; background-color: white; padding:15px; padding-top:60px;"
+
+        var text;
+        if(parent == "myPosts"){
+           text = document.createTextNode("We were unable to display all your posts as some were not public!") 
+        }
+        else{
+             text = document.createTextNode("We were unable to display all the post you are tagged in as some were not public!") 
+        }
+        
+        var text2 = document.createTextNode(skipped + " Posts were not included")
+        p.appendChild(text)
+        p.appendChild(document.createElement("br"))
+        p.appendChild(text2)
+        cntr.appendChild(p)
+        element.appendChild(cntr)
+    }
