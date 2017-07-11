1b89af60ab738e52cafd9a6b69bfc513d1964a29
Ian Donovan <epicpunnum@ymail.com>
2017-07-11 11:33:06 -0400

    Finished up auto-update for user/home timelines
    Notifications needs to be fleshed out but otherwise its lookng good.

--- src/js/twitter/twitter.js
+++ src/js/twitter/twitter.js
@@ -1,3 +1,5 @@
+/* API calls to the PHP backend for Twitter */
+
 function logged_in() {
 	$.ajax({
 		type: "GET",
@@ -19,13 +21,13 @@ function user_credentials() {
 		contentType: "application/text",
 		url: "../../php/twitter/user_credentials.php",
 		success: function( response ) {
-			console.log( "GET twitter/user_credentials: " + response );
+			//console.log( "GET twitter/user_credentials: " + response );
 			var userData = JSON.parse( response );
-            //DEBUGGING
-            if (window.location.href.indexOf("twitter_backend_test") !== -1) {
-                var output = document.getElementById("output");
-                output.innerHTML = JSON.stringify(userData, null, 2);
-            }
+			//DEBUGGING
+			if (window.location.href.indexOf("twitter_backend_test") !== -1) {
+				var output = document.getElementById("output");
+				output.innerHTML = JSON.stringify(userData, null, 2);
+			}
 		},
 		error: function( response ) {
 			window.alert( "ERROR! Couldn't get user credentials from Twitter.\nMake sure you're logged in!" );
@@ -33,19 +35,25 @@ function user_credentials() {
 	});
 }
 
-function notifications() {
+function notifications( callback, args ) {
 	$.ajax({
 		type: "GET",
 		contentType: "application/text",
 		url: "../../php/twitter/notifications.php",
 		success: function( response ) {
-			console.log( "GET twitter/notifications: " + response );
+			//console.log( "GET twitter/notifications: " + response );
 			var notifData = JSON.parse( response );
+			if (callback && typeof callback === "function") {
+				if (args)
+					callback(notifData, args);
+				else
+					callback(notifData);
+			}
 			//DEBUGGING
-            if (window.location.href.indexOf("twitter_backend_test") !== -1) {
-                var output = document.getElementById("output");
-                output.innerHTML = JSON.stringify(notifData, null, 2);
-            }
+			if (window.location.href.indexOf("twitter_backend_test") !== -1) {
+				var output = document.getElementById("output");
+				output.innerHTML = JSON.stringify(notifData, null, 2);
+			}
 		},
 		error: function( response ) {
 			window.alert( "ERROR! Couldn't get notifications from Twitter." );
@@ -53,16 +61,22 @@ function notifications() {
 	});
 }
 
-function home_timeline() {
+function home_timeline( callback, args ) {
 	$.ajax({
 		type: "GET",
 		contentType: "application/text",
 		url: "../../php/twitter/home_timeline.php",
 		success: function( response ) {
-			console.log( "GET twitter/home_timeline: " + response );
+			//console.log( "GET twitter/home_timeline: " + response );
 			var homeData = JSON.parse( response );
+			if (callback && typeof callback === "function") {
+				if (args)
+					callback(homeData, args);
+				else
+					callback(homeData);
+			}
 			//DEBUGGING
-            if (window.location.href.indexOf("twitter_backend_test") !== -1) {
+			if (window.location.href.indexOf("twitter_backend_test") !== -1) {
 				//Outputting embed
 				var embed = document.getElementById("embeds");
 				var inner = '<blockquote class="twitter-tweet" data-lang="en"><p lang="da" dir="ltr">';
@@ -72,9 +86,9 @@ function home_timeline() {
 				inner += '<script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>';
 				embed.innerHTML = inner;
 				//Outputting JSON
-                var output = document.getElementById("output");
-                output.innerHTML = JSON.stringify(homeData, null, 2);
-            }
+				var output = document.getElementById("output");
+				output.innerHTML = JSON.stringify(homeData, null, 2);
+			}
 		},
 		error: function( response ) {
 			window.alert( "ERROR! Couldn't retrieve home page from Twitter." );
@@ -88,7 +102,7 @@ function user_timeline( callback, args ) {
 		contentType: "application/text",
 		url: "../../php/twitter/user_timeline.php",
 		success: function( response ) {
-			console.log( "GET twitter/user_timeline: " + response );
+			//console.log( "GET twitter/user_timeline: " + response );
 			var profileData = JSON.parse( response );
 			if (callback && typeof callback === "function") {
 				if (args)
@@ -97,10 +111,10 @@ function user_timeline( callback, args ) {
 					callback(profileData);
 			}
 			//DEBUGGING
-            if (window.location.href.indexOf("twitter_backend_test") !== -1) {
-                var output = document.getElementById("output");
-                output.innerHTML = JSON.stringify(profileData, null, 2);
-            }
+			if (window.location.href.indexOf("twitter_backend_test") !== -1) {
+				var output = document.getElementById("output");
+				output.innerHTML = JSON.stringify(profileData, null, 2);
+			}
 		},
 		error: function( response ) {
 			window.alert( "ERROR! Couldn't retrieve user timeline from Twitter." );
@@ -108,7 +122,7 @@ function user_timeline( callback, args ) {
 	});
 }
 
-function status_embed( username, tweetID ) {
+function status_embed( username, tweetID, callback, args ) {
 	$.ajax({
 		type: "POST",
 		contentType: "application/text",
@@ -117,11 +131,17 @@ function status_embed( username, tweetID ) {
 		success: function( response ) {
 			console.log( "POST twitter/status_embed: " + response );
 			var embedData = JSON.parse( response );
+			if (callback && typeof callback === "function") {
+				if (args)
+					callback(embedData, args);
+				else
+					callback(embedData);
+			}
 			//DEBUGGING
-      if (window.location.href.indexOf("twitter_backend_test") !== -1) {
-      	var output = document.getElementById("output");
-      	output.innerHTML = JSON.stringify(embedData, null, 2);
-	    }
+			if (window.location.href.indexOf("twitter_backend_test") !== -1) {
+					var output = document.getElementById("output");
+					output.innerHTML = JSON.stringify(embedData, null, 2);
+			}
 		},
 		error: function( response ) {
 			window.alert( "ERROR! Couldn't retrieve user timeline from Twitter." );
@@ -144,10 +164,10 @@ function post_status() {
 			console.log( "POST twitter/post_status: " + response );
 			var postData = JSON.parse( response );
 			//DEBUGGING
-            if (window.location.href.indexOf("twitter_backend_test") !== -1) {
-                var output = document.getElementById("output");
-                output.innerHTML = JSON.stringify(postData, null, 2);
-            }
+			if (window.location.href.indexOf("twitter_backend_test") !== -1) {
+				var output = document.getElementById("output");
+				output.innerHTML = JSON.stringify(postData, null, 2);
+			}
 		},
 		error: function( response ) {
 			window.alert( "ERROR! Couldn't post a status to Twitter." );
@@ -156,32 +176,64 @@ function post_status() {
 }
 
 
-function populate_user_timeline( data, args ) {
+/* Asynchronous list updating functions */
+
+function embed_fallback( tweet ) {
+	//Creates a hard-coded embed for a single tweet object
+	var inner = '<blockquote class="twitter-tweet" data-lang="en"><p lang="da" dir="ltr">';
+	inner += tweet.text + '</p>&mdash; ' + tweet.user.name + ' (@' + tweet.user.screen_name;
+	inner += ') <a href="https://twitter.com/' + tweet.user.screen_name;
+	inner += '/status/' + tweet.id + '">June 28, 2017</a></blockquote>';
+	inner += '<script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>';
+	return {html: inner};
+}
+
+function embed_tweet( data, selector ) {
+	if (data && selector && data.html)
+		$( selector ).append( data.html );
+}
+
+function populate_timeline( data, selector ) {
 	//Iterates through the timeline posts and creates them
 	for (var i=0; i<data.length; i++) {
-
+		//status_embed( data[i].user.screen_name, data[i].id, embed_tweet );
+		embed_tweet( embed_fallback(data[i]), selector );
 	}
-	$("#middleCol").append(  );
+}
+
+function populate_home_timeline( data ) {
+	populate_timeline( data, "#leftCol" );
+}
+
+function populate_user_timeline( data ) {
+	populate_timeline( data, "#middleCol" );
+}
+
+function populate_notifications( data ) {
+	//Iterates through the mentions timeline and creates a list
+	for (var i=0; i<data.length; i++) {
+		$("#rightCol").append( "<p>@"+data.source+" mentioned you in a a tweet!</p>" );
+	}
 }
 
 function update_home_timeline() {
 	const HOME_TIMER = 1000 * 60;
 	console.log("Updating home timeline...");
-
+	home_timeline( populate_home_timeline );
 	window.setTimeout(update_home_timeline, HOME_TIMER);
 }
 
 function update_user_timeline() {
 	const USER_TIMER = 1000 * 60;
 	console.log("Updating user timeline...");
-	user_timeline( populate_user_timeline ) );
+	user_timeline( populate_user_timeline );
 	window.setTimeout(update_user_timeline, USER_TIMER);
 }
 
 function update_notifications() {
 	const NOTIFICATION_TIMER = 1000 * 60;
 	console.log("Updating notifications...");
-
+	notifications( populate_notifications );
 	window.setTimeout(update_notifications, NOTIFICATION_TIMER);
 }
 
@@ -191,6 +243,8 @@ function update_page_init() {
 	update_notifications();
 }
 
+
+/* Functions specifically for the Twitter debug page */
 
 function debug_embed() {
 	if (window.location.href.indexOf("twitter_backend_test") !== -1) {
